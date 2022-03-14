7312fc873a1c6c7857d37eb36dce747bae28acea
Nino Fachrurozy <nfach98@gmail.com>
2022-03-14 14:51:08 +0700

    fix formatting

--- lib/page/home/widget/item_most_popular.dart
+++ lib/page/home/widget/item_most_popular.dart
@@ -97,11 +97,11 @@ class ItemMostPopular extends StatelessWidget {
                         ),
                       ],
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
