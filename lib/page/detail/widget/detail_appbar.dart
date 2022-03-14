5a9dc10036943f7cc5e8d7a3c0667fcb8f5d1a75
Muhammad Isa <iza_helena93@yahoo.co.id>
2022-03-14 17:30:52 +0700

    2 - Image/Slider [#2dmz42p] (#6)
    * Detail AppBar
    
    Appbar halaman detail / 2
    
    * 2 Image/Slider
    
    tambahan image slider
    
    Co-authored-by: Nino Fachrurozy <48285366+nfach98@users.noreply.github.com>

---
+++ lib/page/detail/widget/detail_appbar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class DetailAppBar extends StatefulWidget {
+  final double screenHeight;
+  const DetailAppBar({
+    Key? key,
+    required this.screenHeight,
+  }) : super(key: key);
+
+  @override
+  State<DetailAppBar> createState() => _DetailAppBarState();
+}
+
+class _DetailAppBarState extends State<DetailAppBar> {
+  bool isFavourite = false;
+
+  void favouriteClicked() {
+    isFavourite = !isFavourite;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white.withOpacity(0.4),
+            fixedSize: Size(widget.screenHeight, widget.screenHeight),
+            shape: const CircleBorder(
+              side: BorderSide(color: Colors.white, width: 2),
+            ),
+          ),
+          child: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white.withOpacity(0.4),
+            fixedSize: Size(widget.screenHeight, widget.screenHeight),
+            shape: const CircleBorder(
+              side: BorderSide(color: Colors.white, width: 2),
+            ),
+          ),
+          child: isFavourite
+              ? const Icon(Icons.favorite)
+              : const Icon(Icons.favorite_border),
+          onPressed: () {
+            setState(() {
+              favouriteClicked();
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,62 +1,64 @@
 import 'package:flutter/material.dart';
 
 // Colors
-Color colorPrimary = Color(0xFF416CE1);
-Color colorAccent = Color(0xFFA0B3E2);
+Color colorPrimary = const Color(0xFF416CE1);
+Color colorAccent = const Color(0xFFA0B3E2);
 
-Color colorBackground = Color(0xFFFFFFFF);
-Color colorBackground2 = Color(0xFF7694E2);
+Color colorBackground = const Color(0xFFFFFFFF);
+Color colorBackground2 = const Color(0xFF7694E2);
 
-Color colorIconBnvActive = Color(0xFF6788E2);
-Color colorBackgroundBnv = Color(0xFF6E8DDD);
-Color colorBackgroundBnv2 = Color(0xFF5C7BCD);
+Color colorIconBnvActive = const Color(0xFF6788E2);
+Color colorBackgroundBnv = const Color(0xFF6E8DDD);
+Color colorBackgroundBnv2 = const Color(0xFF5C7BCD);
 
-Color colorBackgroundSearchBar = Color(0xFFEFF2FF);
+Color colorBackgroundSearchBar = const Color(0xFFEFF2FF);
 
-Color colorIconStar = Color(0xFFF5B901);
+Color colorIconStar = const Color(0xFFF5B901);
 
-Color colorTextSearchBar = Color(0xFF8691B3);
-Color colorTextHeading1Dark = Color(0xFF000000);
-Color colorTextHeading1Light = Color(0xFFFFFFFF);
-Color colorTextHeading2 = Color(0xFF313A3F);
-Color colorTextBody1 = Color(0xFFA6A8AA);
-Color colorTextBody2 = Color(0xFF283238);
-Color colorTextBody3 = Color(0xFF7291E2);
+Color colorTextSearchBar = const Color(0xFF8691B3);
+Color colorTextHeading1Dark = const Color(0xFF000000);
+Color colorTextHeading1Light = const Color(0xFFFFFFFF);
+Color colorTextHeading2 = const Color(0xFF313A3F);
+Color colorTextBody1 = const Color(0xFFA6A8AA);
+Color colorTextBody2 = const Color(0xFF283238);
+Color colorTextBody3 = const Color(0xFF7291E2);
 
-Color colorTextButtonLight = Color(0xFFFFFFFF);
-Color colorTextButtonDark = Color(0xFF6688E2);
+Color colorTextButtonLight = const Color(0xFFFFFFFF);
+Color colorTextButtonDark = const Color(0xFF6688E2);
+
+Color colorTextFieldButton = const Color(0xFF7996E2);
 
 // Gradient
 LinearGradient linearGradient = LinearGradient(
-  begin: Alignment.centerLeft,
-  end: Alignment.centerRight,
-  colors: [colorAccent, colorPrimary]
-);
-RadialGradient radialGradient = RadialGradient(
-  colors: [colorAccent, colorPrimary]
-);
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    colors: [colorAccent, colorPrimary]);
+RadialGradient radialGradient =
+    RadialGradient(colors: [colorAccent, colorPrimary]);
 
 LinearGradient backgroundGradient = LinearGradient(
-  begin: Alignment.bottomLeft,
-  end: Alignment.topRight,
-  colors: [colorBackground2, colorBackground]
-);
+    begin: Alignment.bottomLeft,
+    end: Alignment.topRight,
+    colors: [colorBackground2, colorBackground]);
 
 // Text styles
-TextStyle textStyleSearchBar = TextStyle(
-  color: colorTextSearchBar
-);
+TextStyle textStyleSearchBar = TextStyle(color: colorTextSearchBar);
 
-TextStyle textStyleHeading1Light = TextStyle(
-  color: colorTextHeading1Light,
-  fontWeight: FontWeight.bold
-);
-TextStyle textStyleHeading1Dark = TextStyle(
-  color: colorTextHeading1Dark,
-  fontWeight: FontWeight.bold
-);
+TextStyle textStyleHeading1Light =
+    TextStyle(color: colorTextHeading1Light, fontWeight: FontWeight.bold);
+TextStyle textStyleHeading1Dark =
+    TextStyle(color: colorTextHeading1Dark, fontWeight: FontWeight.bold);
 TextStyle textStyleHeading2 = TextStyle(
   color: colorTextHeading2,
+    fontWeight: FontWeight.w600,
+  fontSize: 30,
+);
+
+TextStyle textStyleBody1 =
+    TextStyle(color: colorTextBody1, fontSize: 18, fontWeight: FontWeight.w400);
+TextStyle textStyleBody2 = TextStyle(color: colorTextBody2);
+TextStyle textStyleBody3 = TextStyle(color: colorTextBody3);
+=======
   fontWeight: FontWeight.bold,
   fontSize: 18
 );
@@ -74,9 +76,5 @@ TextStyle textStyleBody3 = TextStyle(
   fontWeight: FontWeight.bold
 );
 
-TextStyle textStyleButtonLight = TextStyle(
-  color: colorTextButtonLight
-);
-TextStyle textStyleButtonDark = TextStyle(
-  color: colorTextButtonDark
-);
+TextStyle textStyleButtonLight = TextStyle(color: colorTextButtonLight);
+TextStyle textStyleButtonDark = TextStyle(color: colorTextButtonDark);
