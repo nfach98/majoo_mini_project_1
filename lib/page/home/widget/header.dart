75387ad38546ee5da3a70f0dcdc5923ffc301f28
rizki <54944887+vstacked@users.noreply.github.com>
2022-03-14 14:26:55 +0700

    feat(parameter): :sparkles: add width parameter, fix formatter

--- lib/page/home/widget/header.dart
+++ lib/page/home/widget/header.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:mini_project_1/utils/constants.dart';
 
 class Header extends StatelessWidget {
-  const Header({Key? key, this.rating = 4.9, this.sales = 542, this.imageName = 'drone_1.png', this.onPressed})
-      : super(key: key);
+  const Header({
+    Key? key,
+    this.rating = 4.9,
+    this.sales = 542,
+    this.imageName = 'drone_1.png',
+    this.onPressed,
+    this.width,
+  }) : super(key: key);
   final double rating;
   final int sales;
   final String imageName;
   final VoidCallback? onPressed;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    final width = this.width ?? MediaQuery.of(context).size.width;
     final textScaleFactor = MediaQuery.of(context).textScaleFactor;
     return Container(
       // TODO need to change the height of the header when width >= 480
@@ -21,7 +28,8 @@ class Header extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         gradient: RadialGradient(
           colors: [colorAccent, colorPrimary],
-          center: width >= 480 ? const Alignment(0, -.5) : const Alignment(.5, 0),
+          center:
+              width >= 480 ? const Alignment(0, -.5) : const Alignment(.5, 0),
           radius: 2.5,
         ),
       ),
@@ -30,7 +38,8 @@ class Header extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 16, 4.5, 16),
             child: Align(
-              alignment: width >= 480 ? Alignment.topCenter : Alignment.centerRight,
+              alignment:
+                  width >= 480 ? Alignment.topCenter : Alignment.centerRight,
               child: Image.asset('assets/images/$imageName'),
             ),
           ),
@@ -56,14 +65,18 @@ class Header extends StatelessWidget {
                       Flexible(
                         child: Text(
                           '$rating ($sales)',
-                          style: textStyleBody1.copyWith(color: colorBackground),
+                          style:
+                              textStyleBody1.copyWith(color: colorBackground),
                           maxLines: 2,
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
                     ],
                   ),
-                  if (width >= 480) const SizedBox(height: 16) else const Spacer(),
+                  if (width >= 480)
+                    const SizedBox(height: 16)
+                  else
+                    const Spacer(),
                   ElevatedButton(
                     onPressed: onPressed ?? () {},
                     child: const Text('Buy Now'),
@@ -72,7 +85,9 @@ class Header extends StatelessWidget {
                       onPrimary: colorPrimary,
                       shadowColor: Colors.transparent,
                       elevation: 0,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
                     ),
                   ),
                 ],
